fb469b1c7c46f882aa8460b10a92cfc685bdeb41
shard520 <65344196+shard520@users.noreply.github.com>
2021-05-05 20:27:45 +0100

    Add sticky nav and section reveal using the intersection observer. Change smooth scrolling method to include offset scroll to account for nav height.

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 
 const nav = document.querySelector('.nav');
+const navHeight = nav.getBoundingClientRect().height;
+
+const header = document.querySelector('.header');
+
+const allSections = document.querySelectorAll('.section');
 
 ///////////////////////////////////////
 // Modal window
@@ -46,7 +51,12 @@ document.addEventListener('keydown', function (e) {
 // Learn more button - smooth scrolling
 
 btnScrollTo.addEventListener('click', function () {
-  section1.scrollIntoView({ behavior: 'smooth' });
+  window.scrollTo({
+    left: 0,
+    top: section1.getBoundingClientRect().top - navHeight + window.pageYOffset,
+    behavior: 'smooth',
+  });
+  nav.classList.add('sticky');
 });
 
 ///////////////////////////////////////
@@ -60,8 +70,14 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     e.target.classList.contains('nav__link') &&
     !e.target.classList.contains('nav__link--btn')
   ) {
+    nav.classList.add('sticky');
     const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    const section = document.querySelector(id);
+    window.scrollTo({
+      left: 0,
+      top: section.getBoundingClientRect().top - navHeight + window.pageYOffset,
+      behavior: 'smooth',
+    });
   }
 });
 
@@ -111,3 +127,42 @@ const handleHover = function (opacity) {
 nav.addEventListener('mouseover', handleHover(0.5));
 
 nav.addEventListener('mouseout', handleHover(1));
+
+///////////////////////////////////////
+// Sticky navigation
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+///////////////////////////////////////
+// Reveal sections
+
+const revealSection = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  section.classList.add('section--hidden');
+  sectionObserver.observe(section);
+});
